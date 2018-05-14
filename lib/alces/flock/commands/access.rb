744024b2407f17b0fb12892961968062bf19ea49
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-05-14 18:43:08 +0100

    Username is optional and doesn't default to `alces`.

--- lib/alces/flock/commands/access.rb
+++ lib/alces/flock/commands/access.rb
@@ -8,11 +8,11 @@ module Alces
       class Access
         def login(args, options)
           cluster_name = args[0]
-          username = args[1] || 'alces'
+          username_args = ['-l',args[1]] if args[1]
           cluster = Cluster.get(cluster_name)
           access_ip = cluster.get('access_ip')
           puts "Access IP: #{access_ip}"
-          Kernel.exec('ssh','-l',username,access_ip)
+          Kernel.exec('ssh',*username_args,access_ip)
         end
       end
     end
